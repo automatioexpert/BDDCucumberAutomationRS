373c7722f732c3b57c9f5a3ce7dd9b1a0384bc05
Valmiki Vishwakarma <you@example.com>
2022-12-08 22:32:31 +0530

    Add.

--- Automation/src/test/java/cucumberOptions/TestRunner.java
+++ Automation/src/test/java/cucumberOptions/TestRunner.java
@@ -5,8 +5,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features",dryRun = false, glue = "steps", tags = "@RegTest",plugin = {"pretty","html:target/cucumber.xml"},
-monochrome = true)
+@CucumberOptions(features = "src/test/java/features", dryRun = false, glue = "steps", tags = "@RegTest", plugin = {
+		"pretty", "html:target/cucumber.xml" }, monochrome = true)
 public class TestRunner {
 
+	
+	
 }
